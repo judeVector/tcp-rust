697849ebefd324b7dba78cb3d5728e4d8f74862f
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2026-03-09 22:35:05 +0100

    add valid segment check

--- src/tcp.rs
+++ src/tcp.rs
@@ -206,6 +206,13 @@ impl Connection {
         // RCV.NXT =< SEG.SEQ+SEG.LEN-1 < RCV.NXT+RCV.WND
 
         let seqn = tcp_header.sequence_number();
+        let mut slen = data.len();
+        if tcp_header.fin() {
+            slen += 1;
+        };
+        if tcp_header.syn() {
+            slen + 1;
+        }
         let wend = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
 
         if data.len() == 0 && !tcp_header.syn() && !tcp_header.fin() {
